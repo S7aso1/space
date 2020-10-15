626f4dcc7dd4ac5acf3b5b024531b472a23834ed
Stanislav <stanislaviliev_97@abv.bg>
2020-10-15 13:29:22 +0300

    add project page

--- Javascript/nodeJS-server/server.js
+++ Javascript/nodeJS-server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 3000;
 var app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -45,6 +45,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
+    })
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'This is the portfolio page'
     })
 })
 

a2f5d0a7677cadb171a9fcc7b3767123cd2bca31
Stanislav <stanislaviliev_97@abv.bg>
2022-01-20 11:39:20 +0200

    remove-submodule

---
+++ fullstack-ecommerce/frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Sector 9',
+            categories: 'longboards',
+            image: '/images/p1.jpg',
+            price: 320,
+            countInStock: 10,
+            brand: 'longboards',
+            rating: 4.5,
+            numReviews: 10,
+            desciption: '34-Inch Cruiser Longboard with multi-ply hardwood maple and beautiful Artisan Bamboo deck'
+        },
+        {
+            _id: '2',
+            name: 'Arbor',
+            categories: 'longboards',
+            image: '/images/p2.jpg',
+            price: 400,
+            countInStock: 30,
+            brand: 'longboards',
+            rating: 5,
+            numReviews: 13,
+            desciption: '34-Inch Cruiser Longboard with multi-ply hardwood maple and beautiful Artisan Bamboo deck'
+        },
+        {
+            _id: '3',
+            name: 'Loaded',
+            categories: 'longboards',
+            image: '/images/p3.jpg',
+            price: 280,
+            countInStock: 0,
+            brand: 'longboards',
+            rating: 5,
+            numReviews: 1,
+            desciption: '34-Inch Cruiser Longboard with multi-ply hardwood maple and beautiful Artisan Bamboo deck'
+        },
+        {
+            _id: '4',
+            name: 'Prism',
+            categories: 'longboards',
+            image: '/images/p4.jpg',
+            price: 330,
+            countInStock: 33,
+            brand: 'longboards',
+            rating: 2.5,
+            numReviews: 30,
+            desciption: '34-Inch Cruiser Longboard with multi-ply hardwood maple and beautiful Artisan Bamboo deck'
+        },
+        {
+            _id: '5',
+            name: 'Dusters',
+            categories: 'longboards',
+            image: '/images/p5.jpg',
+            price: 190,
+            countInStock: 90,
+            brand: 'longboards',
+            rating: 4,
+            numReviews: 10,
+            desciption: '34-Inch Cruiser Longboard with multi-ply hardwood maple and beautiful Artisan Bamboo deck'
+        },
+        {
+            _id: '6',
+            name: 'Santa Cruz',
+            categories: 'longboards',
+            image: '/images/p6.jpg',
+            price: 555,
+            countInStock: 1,
+            brand: 'longboards',
+            rating: 4,
+            numReviews: 15,
+            desciption: '34-Inch Cruiser Longboard with multi-ply hardwood maple and beautiful Artisan Bamboo deck'
+        }
+    ]
+}
+
+export default data
